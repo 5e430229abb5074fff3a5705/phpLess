dbfd57e343e2f88f009707dd52f9d1b08e2de928
MKC <59623220+MKC-MKC@users.noreply.github.com>
2020-03-07 13:39:33 +0300

    Create MySQL.php

---
+++ engine/MySQL.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ * Данные для подключения к БД MYSQL
+ */
+define('DB_Host', 'localhost');
+define('DB_User', 'ПОЛЬЗОВАТЕЛЬ');
+define('DB_Password', 'ПАРОЛЬ');
+define('DB_Name', 'ИМЯ_БАЗЫ_ДАННЫХ');
