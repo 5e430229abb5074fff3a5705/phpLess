c7ac915b5fa15325af359b6a407eb68d6c6a54d6
MKC <59623220+MKC-MKC@users.noreply.github.com>
2020-03-24 00:10:21 +0300

    Less 7 complected

---
+++ auth/index.php
@@ -0,0 +1,15 @@
+<?php require_once '../engine/config.php'; ?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <?php require_once '../engine/head.php'; ?>
+    <title>Авторизация</title>
+</head>
+<body>
+<?php
+    include_once '../engine/navbar.php';
+    require_once 'content.php';
+    include_once '../engine/footer.php'
+?>
+</body>
+</html>
