46df4c1939b9b65bdc3e7fd88e553368bd8a1394
MKC <59623220+MKC-MKC@users.noreply.github.com>
2020-04-05 08:42:34 +0300

    Create content.php
    В тихоря создал меню в сайт.ру/admin

---
+++ admin/content.php
@@ -0,0 +1,5 @@
+<?php echo '
+<a href="'.URL.'">Главная страница сайта</a><br>
+<a href="'.URL.'/admin/list">Список всех пользователей</a><br>
+<a href="'.URL.'/admin/edit">Прямая ссылка на редактор</a><br>
+';
