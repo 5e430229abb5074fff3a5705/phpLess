2e3f82c6c73af809d599cce75eae86bd5a4535dc
MKC <59623220+MKC-MKC@users.noreply.github.com>
2020-04-05 08:46:37 +0300

    Less 10 – Admin – Complected
    Добавлено:
    * - Главная админ панели
    * - Система редактирования пользователя
    * - Система отображения списка пользователей
    
    Короче, сделано всё по ГОСТ от phpLess 😹.

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php require_once '../engine/config.php'; if($user['access'] != 1){header('Location: ' . URL . '/auth'); exit;} ?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <?php require_once '../engine/head.php'; ?>
+    <title>Админ панель</title>
+</head>
+<body>
+<?php
+include_once '../engine/navbar.php';
+require_once 'content.php';
+include_once '../engine/footer.php'
+?>
+</body>
+</html>
